d347855135ebb25e1b61dc65daaa6be0ccb5093b
László Kiss <laszlo.kiss1@ge.com>
2016-07-05 10:00:20 +0200

    authServiceInterface was implemented.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-angular.module("authServiceModule", [])
+var authService = angular.module("authServiceModule", []);
 
-.factory("authService", function () {
+authService.factory("authService", function () {
     return {
 
     }
 });
+
+authService.factory("authServiceInterface", function ($http) {
+    return function(){
+        var token;
+        function login(){
+            token = "bearer 0123456789"
+        };
+
+        function logout(){
+            token = "";
+        };
+
+        function getHttpContext() {
+            return $http({headers: {'Authorization': token}})
+        }
+    }
+});
+
