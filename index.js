d4b983e85fc3d2ea080a147974c81f365d5ae6f6
László Kiss <laszlo.kiss1@ge.com>
2016-07-05 12:59:18 +0200

    Authentication implementation.

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 var authService = angular.module("authServiceModule", []);
 
-authService.factory("authService", function () {
-    return {
+authService.factory("authService", function ($http) {
+    return function(){
+        var token;
+        function login(userName, password){
+            $http({
+                method: 'POST',
+                url: 'localhost:8080/login',
+                data: {"userName": userName, "password": password}
+            }).then(function(data, status, headers){
+                token = headers("Authorization");
+            });
+        };
 
+        function logout(){
+            token = "";
+        };
+
+        function getHttpContext() {
+            return $http({headers: {'Authorization': token}})
+        }
+
+        return {login: login, logout: logout, getHttpContext: getHttpContext};
     }
 });
 
